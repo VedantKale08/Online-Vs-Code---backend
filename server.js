7c0a45d1e0849ca33a43d0d46d5d78b52dffce30
VedantKale08 <fscocomputer@gmail.com>
2024-02-11 20:51:38 +0530

    socket server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connection established ", socket.id);
+
+  socket.on("join-room", (roomId, id, name) => {
+    console.log(`A new user ${name} has joined ${roomId}`);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", id, name);
+  });
+
+  socket.on("user-leave", (userId, roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-leave", userId);
+  });
+
+    socket.on("content-change", (roomId, userId, file) => {
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit("content-change", userId, file);
+    });
+});
+
+httpServer.listen(5000, () => console.log("Server listening on port 5000"));
